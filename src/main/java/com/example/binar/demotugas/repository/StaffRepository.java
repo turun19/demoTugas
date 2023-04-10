50dbdd31a85249b8aefe49f3e7c09794d83cf347
EnVyxS <90240108+EnVyxS@users.noreply.github.com>
2023-04-10 15:15:32 +0700

    menambahkan Query findByUsername dan findByEmail

--- src/main/java/com/example/binar/demotugas/repository/StaffRepository.java
+++ src/main/java/com/example/binar/demotugas/repository/StaffRepository.java
@@ -6,5 +6,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StaffRepository extends JpaRepository<Staff, Integer> {
+  @Query("SELECT s FROM Staff s WHERE LOWER(s.username) LIKE LOWER(concat('%',:username,'%') ) ")
+    List<Staff> findByUsername(@Param("username") String username);
+
+  @Query("SELECT s FROM Staff s WHERE LOWER(s.email) LIKE LOWER(concat('%',:email,'%') ) ")
+    List<Staff> findByEmail(@Param("email") String email);
 
 }
